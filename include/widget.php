4d67ffde19dc472e9f974f41f9706ae5bd9ab421
Coder-Mehedi <mehedi5051@gmail.com>
2019-11-15 12:21:12 +0600

    about page done

--- include/widget.php
+++ include/widget.php
@@ -2,7 +2,7 @@
 
 function bizcola_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Footer One', 'bizcola' ),
+		'name'          => esc_html__( 'Sidebar', 'bizcola' ),
 		'id'            => 'footer-1',
 		'description'   => esc_html__( 'Add widgets here.', 'bizcola' ),
 		'before_widget' => '<div id="%1$s" class="widget %2$s footer-menu">',
@@ -11,35 +11,35 @@ function bizcola_widgets_init() {
 		'after_title'   => '</h5>',
 	) );
 
-	register_sidebar( array(
-		'name'          => esc_html__( 'Footer Two', 'bizcola' ),
-		'id'            => 'footer-2',
-		'description'   => esc_html__( 'Add widgets here.', 'bizcola' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s footer-menu-2">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h5 class="widget-title">',
-		'after_title'   => '</h5>',
-	) );
+	// register_sidebar( array(
+	// 	'name'          => esc_html__( 'Footer Two', 'bizcola' ),
+	// 	'id'            => 'footer-2',
+	// 	'description'   => esc_html__( 'Add widgets here.', 'bizcola' ),
+	// 	'before_widget' => '<div id="%1$s" class="widget %2$s footer-menu-2">',
+	// 	'after_widget'  => '</div>',
+	// 	'before_title'  => '<h5 class="widget-title">',
+	// 	'after_title'   => '</h5>',
+	// ) );
 
-	register_sidebar( array(
-		'name'          => esc_html__( 'Footer Three', 'bizcola' ),
-		'id'            => 'footer-3',
-		'description'   => esc_html__( 'Add widgets here.', 'bizcola' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s footer-menu-2">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h5 class="widget-title">',
-		'after_title'   => '</h5>',
-	) );
+	// register_sidebar( array(
+	// 	'name'          => esc_html__( 'Footer Three', 'bizcola' ),
+	// 	'id'            => 'footer-3',
+	// 	'description'   => esc_html__( 'Add widgets here.', 'bizcola' ),
+	// 	'before_widget' => '<div id="%1$s" class="widget %2$s footer-menu-2">',
+	// 	'after_widget'  => '</div>',
+	// 	'before_title'  => '<h5 class="widget-title">',
+	// 	'after_title'   => '</h5>',
+	// ) );
 
-	register_sidebar( array(
-		'name'          => esc_html__( 'Footer Four', 'bizcola' ),
-		'id'            => 'footer-4',
-		'description'   => esc_html__( 'Add widgets here.', 'bizcola' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s footer-menu-2">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h5 class="widget-title">',
-		'after_title'   => '</h5>',
-	) );
+	// register_sidebar( array(
+	// 	'name'          => esc_html__( 'Footer Four', 'bizcola' ),
+	// 	'id'            => 'footer-4',
+	// 	'description'   => esc_html__( 'Add widgets here.', 'bizcola' ),
+	// 	'before_widget' => '<div id="%1$s" class="widget %2$s footer-menu-2">',
+	// 	'after_widget'  => '</div>',
+	// 	'before_title'  => '<h5 class="widget-title">',
+	// 	'after_title'   => '</h5>',
+	// ) );
 
 }
 add_action( 'widgets_init', 'bizcola_widgets_init' );
--- page-about.php
+++ page-about.php
@@ -1,22 +1,9 @@
-<?php get_header( ); ?>
-    <!-- ==========Page area start ========== -->
-    <div class="page-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="page-content">
-                        <h2 class="wow fadeInDown">About Us</h2>
-                        <ul class="wow fadeInUp">
-                            <li><a href="index.html">Home - </a></li>
-                            <li class="active"><a href="about.html">About us</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<?php require_once 'include/acf_about_page_query.php'; ?>
 
-    <!-- ============= Page area end =========== -->
+<?php get_header( ); ?>
+
+    <?php get_template_part( 'templates/page_top_header' ); ?>
+
     <?php get_template_part( 'templates/about' ); ?>
 
     <?php get_template_part( 'templates/team' ); ?>
@@ -32,12 +19,14 @@
             <div class="row">
                 <div class="col-lg-8 offset-lg-2">
                     <div class="pop-video-content">
-                        <p class="wow fadeInDown">“psum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit “consequat ipsum, </p>
-                        <a data-fancybox href="https://www.youtube.com/watch?v=_sI_Ps7JSEk" class=" wow fadeInUp">
+                        <p class="wow fadeInDown"><?php echo $about_video_section_text; ?></p>
+                    <?php if($about_section_video_link): ?>
+                        <a data-fancybox href="<?php echo $about_section_video_link; ?>" class=" wow fadeInUp">
                             <div class="vi">
                                 <div class="example-1"><i class="fas fa-play"></i></div>
                             </div>
                         </a>
+                    <?php endif; ?>
                     </div>
                 </div>
             </div>
@@ -50,35 +39,24 @@
             <div class="row">
                 <div class="col-lg-8 offset-lg-2">
                     <div class="section-heading">
-                        <h2 class=" wow fadeInDown">Our Project Gallery</h2>
-                        <p class=" wow fadeInUp">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, </p>
+                        <h2 class=" wow fadeInDown"><?php echo $project_gallery_section_heading; ?></h2>
+                        <p class=" wow fadeInUp"><?php echo $project_gallery_section_description; ?></p>
                     </div>
                 </div>
             </div>
+        <?php if($project_gallery_section_gallery_images): ?>
             <div class="row">
                 <div class="col-12  wow fadeInDown">
                     <div class="photo-gallery owl-carousel">
+                    <?php foreach($project_gallery_section_gallery_images as $gallery_single_image): ?>
                         <div class="photo-item">
-                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/gallery/gallery-1.jpg" alt="portlio-image">
+                            <img src="<?php echo $gallery_single_image['url'] ?>" alt="portlio-image">
                         </div>
-                        <div class="photo-item">
-                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/gallery/gallery-2.jpg" alt="portlio-image">
-                        </div>
-                        <div class="photo-item">
-                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/gallery/gallery-3.jpg" alt="portlio-image">
-                        </div>
-                        <div class="photo-item">
-                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/gallery/gallery-1.jpg" alt="portlio-image">
-                        </div>
-                        <div class="photo-item">
-                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/gallery/gallery-2.jpg" alt="portlio-image">
-                        </div>
-                        <div class="photo-item">
-                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/gallery/gallery-3.jpg" alt="portlio-image">
-                        </div>
+                    <?php endforeach; ?>
                     </div>
                 </div>
             </div>
+        <?php endif; ?>
         </div>
     </section>
     <!-- ============= pop video Clients Area End =========== -->
